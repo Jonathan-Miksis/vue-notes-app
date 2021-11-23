c1b2e8bbbb0b8d6f8f5add94f6c38943df4ed978
Jonathan-Miksis <jmiksis13@gmail.com>
2021-11-22 21:21:44 -0500

    pass prop down and emit event up

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -4,6 +4,8 @@
     v-for="note in notes" 
     v-bind:key="note.id"
     v-bind:note="note"
+    v-bind:selectedNoteId="selectedNoteId"
+    v-on:selectNote="selectNote"
     />
   </div>
 </template>
@@ -19,11 +21,17 @@ export default {
 	        { id: 2, body: "This is a second test", timestamp: Date.now() },
 	        { id: 3, body: "This is a third test", timestamp: Date.now() },
 	        { id: 4, body: "This is a fourth test", timestamp: Date.now() }
-	      ]
+	      ],
+        selectedNoteId: 1
 	    };
 	  },
   components: {
     NoteSelector,
+  },
+  methods: {
+    selectNote: function(note) {
+      this.selectedNoteId = note.id;
+    }
   }
 }
 
